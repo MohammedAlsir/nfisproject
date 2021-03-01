beb1d1ff82a8ee1814dbe31d4c3bbb9f0f7ecf5a
sirag <mohamed.sirag5599@hotmail.com>
2021-03-01 11:19:46 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\siragController;
+use App\Http\Controllers\NewsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome')->with(['data'=>3 ,'sirag'=>'fathername']);
+});
+
+
+Route::resource('news','NewsController');
+
